e75cf8b44c1945788b5f595d9900cb7cdd730264
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2021-04-16 13:08:24 -0300

    Creating first component and adding style

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ExpenseItem from './Components/ExpenseItem'
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
