ad168ad6aaf3c76164945fa3421c76a7b4949565
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2021-05-01 13:44:28 -0300

    Adding component for display expenses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import ExpenseItem from './Components/ExpenseItem'
-
+import Expenses from './Components/Expenses'
 
 function App() {
   const expenses = [
@@ -29,26 +28,9 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
 
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date={expenses[0].date}
-      />
-     <ExpenseItem 
-      title={expenses[1].title} 
-      amount={expenses[1].amount} 
-      date={expenses[1].date}
-      />
-       <ExpenseItem 
-      title={expenses[2].title} 
-      amount={expenses[2].amount} 
-      date={expenses[2].date}
-      />
-       <ExpenseItem 
-      title={expenses[3].title} 
-      amount={expenses[3].amount} 
-      date={expenses[3].date}
-       />
+      <Expenses items={expenses} />
+
+      
     </div>
   );
 }
